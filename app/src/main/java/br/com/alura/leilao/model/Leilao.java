4673d211c3f825ef1c0e3c7bd939b1c538cd8503
Robson Lima <rsl.robson@gmail.com>
2020-07-09 23:28:58 -0300

    Implementando as features iniciais: calcular maior e menor lance, exibir maior lance na lista de leilões, exibir maior e menor lance na tela de lances do leilão

--- app/src/main/java/br/com/alura/leilao/model/Leilao.java
+++ app/src/main/java/br/com/alura/leilao/model/Leilao.java
@@ -8,14 +8,34 @@ public class Leilao implements Serializable {
 
     private final String descricao;
     private final List<Lance> lances;
+    private double maiorLance = Double.NEGATIVE_INFINITY; // Atribui o menor valor possível
+    private double menorLance = Double.POSITIVE_INFINITY;
 
     public Leilao(String descricao) {
         this.descricao = descricao;
         this.lances = new ArrayList<>();
     }
 
+    public void propoe(Lance lance){
+        double valorLance = lance.getValor();
+        if (valorLance > maiorLance){
+            maiorLance = valorLance;
+        }
+
+        if (valorLance < menorLance) {
+            menorLance = valorLance;
+        }
+    }
+
     public String getDescricao() {
         return descricao;
     }
 
+    public double getMaiorLance() {
+        return maiorLance;
+    }
+
+    public double getMenorLance() {
+        return menorLance;
+    }
 }
