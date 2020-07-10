54426f0bb81a5ea665c862f0605b6bb010b59993
Robson Lima <rsl.robson@gmail.com>
2020-07-10 11:46:42 -0300

    Adicionando primeiro teste automatizado para a classe Leilao

---
+++ app/src/test/java/br/com/alura/leilao/model/LeilaoTest.java
@@ -0,0 +1,20 @@
+package br.com.alura.leilao.model;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class LeilaoTest {
+
+    @Test
+    public void getDescricao() {
+        // Criar caso de teste
+        Leilao console = new Leilao("Console");
+
+        // Executar ação esperada
+        String descricaoDevolvida = console.getDescricao();
+
+        // Testar resultado esperado
+        assertEquals("Console", descricaoDevolvida);
+    }
+}
