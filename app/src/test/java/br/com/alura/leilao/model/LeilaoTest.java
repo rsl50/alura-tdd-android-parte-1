ea8b3bd9f012dd0ab2fb02d1823eea7d14571221
Robson Lima <rsl.robson@gmail.com>
2020-07-10 11:58:19 -0300

    Adicionando teste para o maior lance

--- app/src/test/java/br/com/alura/leilao/model/LeilaoTest.java
+++ app/src/test/java/br/com/alura/leilao/model/LeilaoTest.java
@@ -17,4 +17,17 @@ public class LeilaoTest {
         // Testar resultado esperado
         assertEquals("Console", descricaoDevolvida);
     }
+
+    @Test
+    public void getMaiorLance() {
+        Leilao console = new Leilao("Console");
+        console.propoe(new Lance(new Usuario("Robson"), 200.0));
+
+        double maiorLanceDevolvido = console.getMaiorLance();
+
+        // delta
+        // verifica a diferença entre os valores de ponto flutuante e se ele for maior,
+        // significa que os valores são equivalentes
+        assertEquals(200.0, maiorLanceDevolvido, 0.0001);
+    }
 }
